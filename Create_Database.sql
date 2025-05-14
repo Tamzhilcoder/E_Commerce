b349e249dac6bdd00bf8e973807a1d27452bebd8
Tamil <tamil21ram@outlook.com>
2025-05-14 08:08:12 +0530

    Customer Table

--- Create_Database.sql
+++ Create_Database.sql
@@ -1,2 +1,10 @@
 create database ecommerce1;
 use ecommerce1;
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Email VARCHAR(100),
+    City VARCHAR(100),
+    State VARCHAR(100),
+    SignupDate DATE
+);
